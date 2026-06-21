28c2d92a35097a5d5f556577a27260e58118c770
proweed001-ux <proweed001@gmail.com>
2026-06-22 00:08:15 +0700

    Use scanner version with separate upload button

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect, useMemo, useState } from 'react';
-import ShiftImageScanner, { ScanResult } from './ShiftImageScanner';
+import ShiftImageScanner, { ScanResult } from './ShiftImageScannerV2';
 import {
   Code,
   Person,
